5d6e1fe64ffa03362ffc2e270ebba000441aafdc
Vladysalv <vladyslav.hutsenko@pollub.edu.pl>
2022-09-04 13:19:40 +0200

    r

---
+++ Asteroids/Assets/Scripts/Installers/PlayerInstaller.cs
@@ -0,0 +1,39 @@
+using BonusesSystem;
+using Entities;
+using Interaction;
+using StatsSystem;
+using StatsSystem.Core;
+using UnityEngine;
+using Utilities.Input;
+using Zenject;
+
+namespace Installers
+{
+    public class PlayerInstaller : MonoInstaller
+    {
+        [SerializeField] private PlayerEntity _playerEntity;
+        [SerializeField] private PlayerInput _playerInput;
+        [SerializeField] private ObjectPicker _objectPicker;
+        
+        public override void InstallBindings()
+        {
+            Container.Bind<Heath>().FromSubContainerResolve().ByMethod(InstallPlayerHealth).AsSingle();
+            Container.Bind<ObjectPicker>().FromSubContainerResolve().ByMethod(InstallPlayer).AsSingle();
+            Container.BindInterfacesAndSelfTo<BonusHandler>().FromSubContainerResolve().ByMethod(InstallPlayer).AsSingle();
+            Container.BindInterfacesAndSelfTo<PlayerEntity>().FromInstance(_playerEntity).AsSingle();
+            Container.Bind<PlayerInput>().FromInstance(_playerInput).AsSingle();
+        }
+
+        private void InstallPlayer(DiContainer subContainer)
+        {
+            subContainer.Bind<ObjectPicker>().FromInstance(_objectPicker).AsSingle();
+            subContainer.BindInterfacesAndSelfTo<BonusHandler>().AsSingle();
+        }
+
+        private void InstallPlayerHealth(DiContainer subContainer)
+        {
+            subContainer.Bind<Heath>().AsSingle();
+            subContainer.BindInstance(_playerEntity.StatsData.GetStat(Stats.Health)).AsSingle();
+        }
+    }
+}
