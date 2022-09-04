6b1b0ba7b0c87db8ed71137c2c8f5a6e32ffdc21
Vladysalv <vladyslav.hutsenko@pollub.edu.pl>
2022-09-04 13:00:28 +0200

    Refactoring

--- Asteroids/Assets/Scripts/Installers/PlayerInstaller.cs
+++ Asteroids/Assets/Scripts/Installers/PlayerInstaller.cs
@@ -1,5 +1,7 @@
 using AsteroidsZenject;
+using BonusesSystem;
 using Entities;
+using Interaction;
 using StatsSystem;
 using StatsSystem.Core;
 using UnityEngine;
@@ -12,15 +14,23 @@ namespace Installers
     {
         [SerializeField] private PlayerEntity _playerEntity;
         [SerializeField] private PlayerInput _playerInput;
-
+        [SerializeField] private ObjectPicker _objectPicker;
         public override void InstallBindings()
         {
             var difficultyData = Container.Resolve<DifficultyManager>().DifficultyData;
             
             Container.BindInstance(difficultyData.PlayerSettingsSo);
             Container.Bind<Heath>().FromSubContainerResolve().ByMethod(InstallPlayerHealth).AsSingle();
+            Container.Bind<ObjectPicker>().FromSubContainerResolve().ByMethod(InstallPlayer).AsSingle();
+            Container.BindInterfacesAndSelfTo<BonusHandler>().FromSubContainerResolve().ByMethod(InstallPlayer).AsSingle();
             Container.BindInterfacesAndSelfTo<PlayerEntity>().FromInstance(_playerEntity).AsSingle();
             Container.Bind<PlayerInput>().FromInstance(_playerInput).AsSingle();
+        }
+
+        private void InstallPlayer(DiContainer subContainer)
+        {
+            subContainer.Bind<ObjectPicker>().FromInstance(_objectPicker).AsSingle();
+            subContainer.BindInterfacesAndSelfTo<BonusHandler>().AsSingle();
         }
 
         private void InstallPlayerHealth(DiContainer subContainer)
